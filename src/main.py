6f7c5d5fa9092b982f1d174735e39fd974088d90
andreped <andrped94@gmail.com>
2022-08-19 17:41:22 +0200

    merged all csvs into a single dataframe for further analysis

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+import statsmodels.api as sm
 
 
 def sort_timestamps(input_):
@@ -83,15 +84,44 @@ def preprocess(data_path):
         # for each time stamp, all clusters and sum these into one value (total tumor amount in ml)
         for timestamp in unique_timestamps:
             tmp = curr_data[curr_data["Timestamp"] == timestamp]
-            curr_volume = sum(list(tmp["Volume"]))
+            tmp = np.nan_to_num(tmp["Volume"])  # convert 0 to NaN for summation
+            curr_volume = sum(list(tmp))
             data.append([pat, timestamp, curr_volume])
             iter += 1
     data = np.array(data)
 
     # merge this with the cohort volumes quality stuff
-    full_data = np.concatenate([filtered_cohort_volumes_quality, data[:, 1:]], axis=1)
+    #full_data = np.concatenate([filtered_cohort_volumes_quality, data[:, 2:3]], axis=1)
+    full_data = filtered_cohort_volumes_quality.copy()
+    full_data["Volumes"] = data[:, -1]
 
     print(full_data.shape)
+    print(full_data)
+
+    # indeces = full_data.index
+
+    # add patient characteristics to full data frame
+    full_data["Birth_Year"] = (-999 * np.ones(full_data.shape[0])).astype("str")
+    full_data["Gender"] = (-999 * np.ones(full_data.shape[0])).astype("str")
+    for pat, gender, byear in zip(cohort_personal_info_filtered["Patient"], cohort_personal_info_filtered["Gender"], cohort_personal_info_filtered["Birth_Year"]):
+        row_ids = np.where(full_data["Patient"] == pat)[0]
+        #if type(row_ids) is not list():
+        #    row_ids = [row_ids]
+        #print(full_data["Patient"] == pat)
+        #print("row ids:", row_ids)
+        for r in row_ids:
+            #print("r:", r)
+            full_data.loc[r + 60, 'Birth_Year'] = byear  # @FIXME: Need better solution, quick-fix: 60 off in row ID...
+            full_data.loc[r + 60, 'Gender'] = gender
+
+    print(full_data)
+    print(full_data.head(30))
+
+    ### Make scatter plots and fit curve (regression)
+    # - 1) test linear regression
+    #model = sm.GLM(np.array(full_data.Volumes).astype(float), pd.get_dummies(full_data.Gender))  # , family=sm.families.Gamma())
+    #results = model.fit()
+    #print(results.summary())
 
 
 if __name__ == "__main__":
